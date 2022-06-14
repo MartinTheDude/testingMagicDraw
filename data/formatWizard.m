9cf45abb9ded391fa531baa338a36a651c535734
MartinTheDude <martinmez727@gmail.com>
2022-06-13 21:31:32 -0400

    Commenting my code

--- data/formatWizard.m
+++ data/formatWizard.m
@@ -1,15 +1,22 @@
 function formatWizard() 
 
+
+%Initialize Filename
 filename = 'octokit.csv';
 
+%Read From CSV
 commits = readtable(filename);
 
+%Take out headers, they are actually apart of the dataset
 imposter = commits.Properties.VariableDescriptions;
 
+%Concat piece of data that was used as header to table
 commits = array2table([cell(imposter) ; table2cell(commits)]);
 
+%Rename Headers
 commits.Properties.VariableNames = ["Commit_ID", "Time/Date", "Author", "Subject"];
 
-writetable(commits, filename);
+%Write Out
+writetable(commits, 'commits.csv');
 
 end
