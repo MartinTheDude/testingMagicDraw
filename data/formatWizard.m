b19ae541f3d3c5fdf9db3e319271f72af9090bd4
MartinTheDude <60444636+MartinTheDude@users.noreply.github.com>
2022-06-15 11:56:56 -0400

    Update formatWizard.m

--- data/formatWizard.m
+++ data/formatWizard.m
@@ -11,7 +11,7 @@ imposter = commits.Properties.VariableDescriptions;
 commits = array2table([cell(imposter) ; table2cell(commits)]);
 
 %Rename Headers
-commits.Properties.VariableNames = ["Commit_ID", "Time/Date", "Author", "Subject", "FileName"];
+commits.Properties.VariableNames = [ "FileName", "Commit_ID", "Time/Date", "Commit_Author" ,"File_Author", "Subject"];
 
 %Write Out
 writetable(commits, 'commits.csv');
