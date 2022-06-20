c64b5bf84aeb93e3a8680dfa673cd8a4687b8e58
MartinTheDude <martinmez727@gmail.com>
2022-06-20 16:11:44 -0400

    update

--- noMagicAPI/src/imgExporter.java
+++ noMagicAPI/src/imgExporter.java
@@ -1,4 +1,8 @@
 
-public class imgExporter {
 
+public class imgExporter {
+	
+	public static void getImg() {
+		img = com.nomagic.export.image.ImageExporter("model", 2, "testingGenericTable");
+	}
 }
